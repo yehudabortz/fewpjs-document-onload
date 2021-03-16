a8a2121a6e3ff5885a9edf9ff7130a339f2031d6
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-15 19:20:44 -0500

    commit tree a96dad76e0586cb4349be2dfc07a051a2b45777e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+console.log(
+    "this log"
+)
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = "This is really cool!"
+})
